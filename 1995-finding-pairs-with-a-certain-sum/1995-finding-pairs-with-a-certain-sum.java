af073deb4e0dc5148c35c6f66c4768b3f982234f
Riya-Jaiswal22 <142654496+Riya-Jaiswal22@users.noreply.github.com>
2025-05-29 21:58:09 +0530

    Time: 184 ms (7.56%), Space: 79.8 MB (17.13%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,32 @@
+class FindSumPairs {
+    int[] nums1,nums2;
+    HashMap<Integer,Integer> map = new HashMap<>();
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        for(int x:nums2){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        map.put(nums2[index],map.getOrDefault(nums2[index],0)-1);
+        nums2[index] += val;
+        map.put(nums2[index],map.getOrDefault(nums2[index],0)+1);
+    }
+    
+    public int count(int tot) {
+        int c=0;
+        for(int x:nums1){
+            c += map.getOrDefault(tot-x,map.getOrDefault(tot-x,0));
+        }
+        return c;
+    }
+}
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs obj = new FindSumPairs(nums1, nums2);
+ * obj.add(index,val);
+ * int param_2 = obj.count(tot);
+ */
